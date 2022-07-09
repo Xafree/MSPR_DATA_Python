b68c48f72a95df7e7adec2ba826b143ac5b3eb77
emmanueleberst@hotmail.com <Xafree1234>
2022-07-09 23:38:49 +0200

    Adding OpenData of cars park about Montpellier

---
+++ OpenData/Montpellier.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+import json
+import os
+
+from dotenv import load_dotenv
+from xml.etree import ElementTree
+
+load_dotenv()
+
+MONTPELLIER = os.getenv('MONTPELLIER')
+
+#On récupère les données de L'api de OpenData de Montpellier
+response = requests.get(MONTPELLIER)
+
+#print(response.json())
+OpenDataMontpellier = response.json()
+
+#On parcoure les données de l'API
+urlList = []
+
+#On parcour les donnée de l'api
+for i in range(len(OpenDataMontpellier['result']['resources'])):
+    urlList.append(OpenDataMontpellier['result']['resources'][i]['url'])
+
+data_montpellier = []
+
+for i in range(2, len(urlList)):
+    res_xml = requests.get(urlList[i])
+    if res_xml.content.decode("utf-8") != "":
+        root = ElementTree.fromstring(res_xml.content)
+        data_set = {
+          "ville": "Montpellier",
+          "nom": root.findtext("Name"),
+          "date": root.findtext("DateTime"),
+          "place_libres": root.findtext("Free"),
+          "places_totales": root.findtext("Total")
+        }
+    data_to_json = json.dumps(data_set)
+    data_montpellier.append(data_to_json)
+
+print(data_montpellier)
